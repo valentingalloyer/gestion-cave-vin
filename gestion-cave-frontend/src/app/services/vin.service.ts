3b85274cc6c6829304a4c68300766cc12d69edbb
Valentin <valentin.galloyer@gmail.com>
2026-01-02 19:44:25 +0100

    Gestion déploiement front

--- gestion-cave-frontend/src/app/services/vin.service.ts
+++ gestion-cave-frontend/src/app/services/vin.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vin } from '../models/vin.model';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable({ providedIn: 'root' })
 export class VinService {
-  private apiUrl = 'http://localhost:8080/api/vins';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
--- gestion-cave/src/main/java/com/beaupapa/gestioncave/controller/VinController.java
+++ gestion-cave/src/main/java/com/beaupapa/gestioncave/controller/VinController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/vins")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = { "http://localhost:4200", "https://gestion-cave-vin.vercel.app/" })
 public class VinController {
 
     @Autowired
